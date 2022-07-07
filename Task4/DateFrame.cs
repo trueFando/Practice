ddd66880f8b4cd7f1574996f3b3f8f6e118873b3
trueFando <work_fando@mail.ru>
2022-07-07 11:04:15 +0300

    начало 4-й задачи

---
+++ Task4/DateFrame.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    struct DateFrame
+    {
+        public string DateAndTme
+        {
+            get;
+            private set;
+        }
+
+        public string Frame
+        {
+            get;
+            private set;
+        }
+
+        public DateTime Date
+        {
+            get
+            {
+                string date = DateAndTme.Substring(0, DateAndTme.IndexOf('T'));
+
+                return DateTime.Parse(date);
+            }
+        }
+
+        public int Milliseconds
+        {
+            get
+            {
+                string time = DateAndTme.Substring(DateAndTme.IndexOf('T') + 1,
+                    DateAndTme.Length - DateAndTme.Substring(0, DateAndTme.IndexOf('T')).Length - 1);
+                DateTime dt = DateTime.Parse(time).AddHours(-3);
+                return (int) dt.TimeOfDay.TotalMilliseconds;
+            }
+        }
+
+        public int DaysFrom1900
+        {
+            get
+            {
+                DateTime dt1900 = new DateTime(1900, 1, 1);
+                return (int)(Date - dt1900).TotalDays;
+            }
+        }
+
+        public void SetDateAndFrame(string date, string frame)
+        {
+            DateAndTme = date;
+            Frame = frame;
+        }
+    }
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.RegularExpressions;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // получаем путь к файлу входных данных
+            string filePath = ConfigurationManager.AppSettings.Get("inputFilePath");
+
+            DateFrame[] dateFrames;
+            int numberOfFrames;
+            // создаем поток для чтения
+            using (StreamReader streamReader = new StreamReader(filePath))
+            {
+                // размерность массива
+                numberOfFrames = NumberOfFrames(streamReader.ReadToEnd());
+
+                // сюда сохраняем все строчки
+                string[] allData = new string[numberOfFrames];
+                
+                // из allData собираем массив своей структуры
+                dateFrames = new DateFrame[numberOfFrames];
+
+                // ставим указатель в начало
+                streamReader.BaseStream.Seek(0, SeekOrigin.Begin);
+
+
+                for (int i = 0; i < numberOfFrames; i++)
+                {
+                    // читаем строчку с датой+временем и кадром
+                    allData[i] = streamReader.ReadLine();
+
+                    // отделяем дату+время от кадра
+                    string date = allData[i].Substring(0, allData[i].IndexOf(' '));
+                    string frame = allData[i].Substring(allData[i].IndexOf(' ') + 1, allData[i].Length - date.Length - 1);
+
+                    // проверяем на наличие игнорируемой последовательности
+                    if (frame.Substring(0, 8) == "1ACFFC1D") 
+                        frame = frame.Substring(8, frame.Length-8);
+
+                    // записываем элемент в массив
+                    dateFrames[i].SetDateAndFrame(date, frame);
+                }
+            }
+        }
+
+        // ищем количество переносов строки в input - столько у нас фреймов
+        private static int NumberOfFrames(string data)
+        {
+            return new Regex("\n").Matches(data).Count;
+        }
+    }
+}
